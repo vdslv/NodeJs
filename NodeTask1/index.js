3e1ebf94e5e73cc3fcf8a18f8fa52c48b07d4ecd
Vladyslav Havryliak <vladyslav_havryliak@epam.com>
2020-05-19 00:35:47 +0300

    task done

---
+++ NodeTask1/index.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const fs = require("fs");
+
+const changeFilePosition = (arrOfFiles, cameFrom, goingTo) => {
+  arrOfFiles.forEach((file) => {
+    fs.rename(
+      path.join(`${cameFrom}`, `${file}`),
+      path.join(`${goingTo}`, `${file}`),
+      (err) => {
+        if (err) {
+          console.log(err);
+        }
+      }
+    );
+    console.log(file, cameFrom);
+  });
+};
+
+const getArrOfFiles = (folder, putFolder) => {
+  const directory = path.join(__dirname, folder);
+  fs.readdir(directory, (err, files) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    changeFilePosition(files, directory,putFolder);
+  });
+};
+
+getArrOfFiles("1800", "2000");
+getArrOfFiles("2000", "1800");
